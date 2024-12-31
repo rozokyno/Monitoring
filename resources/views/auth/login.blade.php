440e25be7d8dc7750985874b2bcb61a58b257ff2
Jekoyu <jekoyu157@gmail.com>
2024-12-31 09:20:38 +0700

    fix : login be

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,14 +9,9 @@
     <meta name="author" content="">
 
     <title>Login</title>
-
-    <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <style>
         body {
@@ -40,45 +35,32 @@
 </head>
 
 <body>
-
-    <div class="card-custom">
-        <div class="text-center">
-            <h1 class="h4 text-gray-900 mb-4">Monitoring Jaringan SD Negeri Banguntapan</h1>
-        </div>
-        <form class="user">
-            <div class="form-group">
-                <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                    aria-describedby="emailHelp" placeholder="Enter Email Address...">
-            </div>
-            <div class="form-group">
-                <input type="password" class="form-control form-control-user" id="exampleInputPassword"
-                    placeholder="Password">
-            </div>
-            <div class="form-group">
-                <div class="custom-control custom-checkbox small">
-                    <input type="checkbox" class="custom-control-input" id="customCheck">
-                    <label class="custom-control-label" for="customCheck">Remember Me</label>
-                </div>
-            </div>
-            <a href="{{ route('admin.dashboard.index') }}" class="btn btn-primary btn-user btn-block">
-                Login
-            </a>
-        </form>
-        <hr>
-        <div class="text-center">
-            <a class="small" href="forgot-password.html">Forgot Password?</a>
-        </div>
-        <div class="text-center">
-            <a class="small" href="register.html">Create an Account!</a>
-        </div>
+<div class="card-custom">
+    <div class="text-center">
+        <h1 class="h4 text-gray-900 mb-4">Monitoring Jaringan SD Negeri Banguntapan</h1>
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
-
+    <form class="user" method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="form-group">
+            <input type="email" name="email" class="form-control form-control-user"
+                   placeholder="Enter Email Address..." required>
+        </div>
+        <div class="form-group">
+            <input type="password" name="password" class="form-control form-control-user"
+                   placeholder="Password" required>
+        </div>
+        <button type="submit" class="btn btn-primary btn-user btn-block">
+            Login
+        </button>
+    </form>
+    <hr>
+    <div class="text-center">
+        <a class="small" href="#">Forgot Password?</a>
+    </div>
+    <div class="text-center">
+        <a class="small" href="#">Create an Account!</a>
+    </div>
+</div>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,25 @@
 use App\Http\Controllers\ComputerController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+// Route::get('/', function () {
+//     return view('auth.login');
+// });
+
+// Route::get('/login', function () {
+//     return view('auth.login');
+// });
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
 
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/dashboard', [DashboardController::class,'index'])->name('admin.dashboard.index');
-
-Route::get('/speed', [DashboardController::class,'speed'])->name('speedtest.run');
+Route::get('/speed', [DashboardController::class, 'speed'])->name('speedtest.run');
 
 Route::resource('tables', ComputerController::class);
 
